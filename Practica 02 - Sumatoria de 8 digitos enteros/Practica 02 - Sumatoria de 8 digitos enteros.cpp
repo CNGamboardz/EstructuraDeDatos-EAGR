3fb022d21b8925ba325ec0f4b8cc09f878b281d8
Erick Gamboa <138053722+CNGamboardz@users.noreply.github.com>
2023-10-02 20:15:48 -0600

    Practica 02 - Sumatoria de 8 dígitos enteros-C++

---
+++ Practica 02 - Sumatoria de 8 digitos enteros/Practica 02 - Sumatoria de 8 digitos enteros.cpp
@@ -0,0 +1,19 @@
+/*Erick Gamboa
+Hacer un programa de arreglos en Visual Studio Code usando C++*/
+#include <iostream>
+using namespace std;
+const int num = 8;
+
+int main()
+{
+    int nums[num];
+    int total = 0;
+    for (int i = 0; i < num; i++)
+    {
+        cout << "Por favor, Introduza el numero: ";
+        cin >> nums[i];
+        total += nums[i];
+    }
+    cout << "El total de numero es: " << total << endl;
+    return 0;
+}
