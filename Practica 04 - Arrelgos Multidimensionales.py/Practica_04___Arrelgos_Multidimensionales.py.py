b0b86d3a29abc02ff13aafc84372733d48e7c42e
Erick Gamboa <138053722+CNGamboardz@users.noreply.github.com>
2023-10-02 21:12:00 -0600

    Practica 04 - Arrelgos Multidimensionales-py

---
+++ Practica 04 - Arrelgos Multidimensionales.py/Practica_04___Arrelgos_Multidimensionales.py.py
@@ -0,0 +1,20 @@
+#Erick Gamboa
+#Practica 04 - Arrelgos Multidimensionales
+
+maxf = 3
+maxc = 5
+a = [[0] * maxc for _ in range(maxf)]
+
+print("LIDS 3J")
+ #Escribir el array 
+for f in range(maxf):
+  for c in range(maxc):
+    print("Ingresa el valor: ")
+    a[f][c] = int (float(input()))
+
+#Leer el array
+print (" Los valores del arreglo son los siguientes: ")
+for f in range(maxf):
+    for c in range(maxc):
+      print (a[f][c], end=" ")
+      print()
