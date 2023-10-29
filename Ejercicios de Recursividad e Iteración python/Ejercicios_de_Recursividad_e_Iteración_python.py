c356fcba0b54f3fe03dec4f7b08634fcda8ddb65
Erick Gamboa <138053722+CNGamboardz@users.noreply.github.com>
2023-10-29 12:11:44 -0600

    Ejercicios de Recursividad e Iteración phyton

---
+++ Ejercicios de Recursividad e Iteración python/Ejercicios_de_Recursividad_e_Iteración_python.py
@@ -0,0 +1,10 @@
+#Erick Gamboa
+#Ejercicios de Recursividad e Iteración
+
+
+NUM = int(input("DIGITE NUMERO: "))
+for I in range(1, 13):
+    RESULT = NUM * I
+    print(f"{NUM} * {I} = {RESULT}")
+input("Pulse una tecla: ")
+
