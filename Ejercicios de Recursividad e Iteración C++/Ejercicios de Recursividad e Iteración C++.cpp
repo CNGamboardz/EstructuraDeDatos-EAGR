77d1560867c603e595627ee127723eb54e09ddd1
Erick Gamboa <138053722+CNGamboardz@users.noreply.github.com>
2023-10-29 12:11:09 -0600

    Ejercicios de Recursividad e Iteración C++

---
+++ Ejercicios de Recursividad e Iteración C++/Ejercicios de Recursividad e Iteración C++.cpp
@@ -0,0 +1,24 @@
+//Erick Gamboa
+//Ejercicios de Recursividad e Iteración
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int NUM, I, RESULT;
+    string linea;
+    cout << "DIGITE NUMERO: ";
+    getline(cin, linea);
+    NUM = stoi(linea);
+    for (I = 1; I <= 12; I++)
+    {
+        RESULT = NUM * I;
+        cout << NUM << " * " << I << " = " << RESULT << endl;
+    }
+    cout << "Pulse una tecla: ";
+    cin.ignore();
+    return 0;
+}
